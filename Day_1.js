b3ad66823534dd7629d6449496e830a1345be27e
Ankit Lodhi <134636170+AnkitLodhi2002@users.noreply.github.com>
2024-02-04 15:26:49 +0530

    Day_1 Problem complete

---
+++ Day_1.js
@@ -0,0 +1,74 @@
+//CAP-08 (Introduction to JS - Variables & Data Types)
+
+//Problem 1
+
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+//Problem 2
+
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+var _name = "Ankit Lodhi"
+console.log(_name)
+var father_name = "Badri Prasad lodhi"
+console.log(father_name)
+var mother_name = "Radha Bai Lodhi"
+console.log(mother_name)
+
+//Problem 3
+
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+var _name = "Ankit Lodhi"
+var age = 21
+console.log(_name)
+console.log(age)
+console.log(typeof(_name))
+console.log(typeof(age))
+
+//Problem 4
+
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+var _name = "Ankit Lodhi"
+var school = "School for Excellence Bhopal"
+var grade = 10
+var section = "A"
+var roll_no = 60
+var Hindi = 82
+var Math = 97
+var English = 60
+console.log("_name :",_name)
+console.log("school :", school)
+console.log("grade :", grade)
+console.log("section :", section)
+console.log("roll_no :", roll_no)
+console.log("Hindi :", Hindi)
+console.log("Math :", Math)
+console.log("English :", English)
+console.log("████████████████████████████████████████████████████████████████████████████████")
+console.log("█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░█")
+console.log("█░░▄▀░░░░░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█")
+console.log("█░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░█░░░░▄▀░░░░█")
+console.log("█░░▄▀░░░░░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░███░░▄▀░░███")
+console.log("█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███")
+console.log("█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░███")
+console.log("█░░▄▀░░██░░░░░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░███░░▄▀░░███")
+console.log("█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░█████████░░▄▀░░█░░▄▀░░██░░▄▀░░███░░▄▀░░███")
+console.log("█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░░░░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░█░░░░▄▀░░░░█")
+console.log("█░░▄▀░░██████████░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█")
+console.log("█░░░░░░██████████░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░█░░░░░░░░░░█")
+console.log("████████████████████████████████████████████████████████████████████████████████")
+

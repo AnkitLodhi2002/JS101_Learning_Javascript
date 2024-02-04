4a6505384ff962019edfd72896282c09a357e798
Ankit Lodhi <134636170+AnkitLodhi2002@users.noreply.github.com>
2024-02-04 15:34:52 +0530

    Day_2

---
+++ Day_2.js
@@ -0,0 +1,48 @@
+//CAP-08 (IW- Mathematical operators )
+
+//problem 1
+
+//Multiply by 50
+
+let n =  2;
+console.log(n*50)
+
+//problem 2
+
+//Product of Six
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+console.log(one*two*three*four*five*six)
+
+//problem 3
+
+//Sum of five
+
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+console.log(n1+n2+n3+n4+n5)
+
+//problem 4
+//Cube of a number
+let N = 4;
+console.log(N**3)
+
+//problem 5
+//Find X
+let X = 2
+var multiply = X*3
+console.log(multiply+10)
+
+//problem 6
+//Square and Sum
+let num1 = 1
+let num2 = 2
+let num3 = 3
+console.log((num1**2)+(num2**2)+(num3**2))
